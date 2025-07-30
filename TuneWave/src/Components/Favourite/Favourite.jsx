bac33808004ee351426da8670179fd9dedd0106a
aakashKumar668 <akparjapati2022@gmail.com>
2025-07-30 16:25:19 +0530

    it's new music app

---
+++ TuneWave/src/Components/Favourite/Favourite.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { BsMusicNoteList } from "react-icons/bs";
+import Song from '../Song/Song';
+import { SongContext } from '../../Context/Context'; // adjust the path
+
+const SongContainer = () =>
+{
+    const { setSelectedIndex, setSongList } = useContext(SongContext);
+    const [favSongs, setFavSongs] = useState([]);
+
+    useEffect(() =>
+    {
+        const saved = JSON.parse(localStorage.getItem("favSong")) || [];
+        setFavSongs(saved);
+        setSongList(saved); // 💡 Set this so Player has the correct list
+    }, []);
+
+    return (
+        <div className='w-3/7 rounded shadow-lg/100 shadow-blue-600 ring-3 ring-white'>
+            <div className='text-4xl text-white border-b-4 border-gray-600 py-3 px-5 font-[cursive] font-bold flex items-center gap-2 '><BsMusicNoteList className='text-green-500' /><h1>Favourite Songs...</h1>
+            </div>
+
+            <div className='mt-3 h-80 p-4 overflow-auto  [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 '>
+
+
+                {favSongs.map((item, i) => <Song image={item.img} name={item.name} artist={item.artist} key={i} index={i} setSelectedIndex={setSelectedIndex} />)}
+
+
+            </div>
+        </div>
+    )
+}
+
+export default SongContainer
---
+++ TuneWave/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='bg-gray-800 text-gray-400 font-bold flex justify-center items-center text-xl h-10 w-full'>
+        <h1> &copy; 2025 TuneWave || All Right Reserved</h1>
+    </div>
+    </>
+  )
+}
+
+export default Footer
